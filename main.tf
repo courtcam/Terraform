01822f56b8c3c7c2f6db190be7ec6044a764bb4d
courtcam <38297090+courtcam@users.noreply.github.com>
2023-04-05 00:50:07 -0400

    added a bash script to run a boot in my user data file

--- main.tf
+++ main.tf
@@ -24,17 +24,7 @@ resource "aws_instance" "Jenkins" {
   }
   
   
-	user_data = <<EOF
-        #!/bin/bash
-		 curl -fsSL https://pkg.jenkins.io/debian/jenkins.io-2023.key | sudo tee \
-        /usr/share/keyrings/jenkins-keyring.asc > /dev/null
-	    echo deb [signed-by=/usr/share/keyrings/jenkins-keyring.asc] \
-        https://pkg.jenkins.io/debian binary/ | sudo tee \
-        /etc/apt/sources.list.d/jenkins.list > /dev/null
-        sudo apt-get update
-        sudo apt-get install fontconfig openjdk-11-jre -y
-        sudo apt-get install jenkins -y
-	EOF
+	user_data = file("jenkins.sh")
 }
 
 
