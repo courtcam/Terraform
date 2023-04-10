4d4a8046b2f7c0d5f0fc462a3fdae55230374e82
courtcam <38297090+courtcam@users.noreply.github.com>
2023-04-10 13:25:09 -0400

    Adjusted subnets and added public ip on launch

---
+++ auto-scaling/variables.tf
@@ -0,0 +1,16 @@
+
+# MY instance AMI 
+
+variable "instance_ami" {
+  type    = string
+  default = "ami-0fa1de1d60de6a97e "
+}
+
+
+
+# MY instance Type
+
+variable "instance_type" {
+  type    = string
+  default = "t2.micro"
+}
